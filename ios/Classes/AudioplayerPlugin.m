329110d67042ed08800349cd30f9d9fdce259fb4
Roma Osiychuk <osiychukr@gmail.com>
2019-11-19 18:39:44 +0200

    fix crash

--- ios/Classes/AudioplayerPlugin.m
+++ ios/Classes/AudioplayerPlugin.m
@@ -54,17 +54,17 @@ MPRemoteCommandCenter *commandCenter;
     commandCenter = [MPRemoteCommandCenter sharedCommandCenter];
     
     [commandCenter.pauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent *event) {
-    
-        [instance pause];
-        CMTime duration = [[player currentItem] currentTime];
-        [instance setPlaybackInfo:(int)CMTimeGetSeconds(duration)];
+
+        [player pause];
+        isPlaying = false;
+        [_channel invokeMethod:@"audio.onSELPause" arguments:nil];
         
         return MPRemoteCommandHandlerStatusSuccess;
     } ];
     
     [commandCenter.playCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent *event) {
         CMTime duration = [[player currentItem] duration];
-        [[player currentItem] addObserver:instance
+        [[player currentItem] addObserver:self
                                forKeyPath:@"player.currentItem.status"
                                   options:0
                                   context:nil];
@@ -72,13 +72,12 @@ MPRemoteCommandCenter *commandCenter;
         if (CMTimeGetSeconds(duration) > 0) {
             int mseconds= CMTimeGetSeconds(duration)*1000;
             [_channel invokeMethod:@"audio.onStart" arguments:@(mseconds)];
-        } else {
-           [_channel invokeMethod:@"audio.onContinue" arguments:nil];
         }
         
         [player play];
         isPlaying = true;
-        
+        [_channel invokeMethod:@"audio.onContinue" arguments:nil];
+
         return MPRemoteCommandHandlerStatusSuccess;
     } ];
     
@@ -249,9 +248,10 @@ MPRemoteCommandCenter *commandCenter;
                                                                       }];
         [observers addObject:anobserver];
 
-//        if (player) {
+        if (player) {
 //            [player replaceCurrentItemWithPlayerItem:playerItem];
-//        } else {
+            player = [[AVPlayer alloc] initWithPlayerItem:playerItem];
+        } else {
             player = [[AVPlayer alloc] initWithPlayerItem:playerItem];
             // Stream player position.
             // This call is only active when the player is active so there's no need to
@@ -261,7 +261,7 @@ MPRemoteCommandCenter *commandCenter;
                 [self onTimeInterval:time];
             }];
             [timeobservers addObject:timeObserver];
-//        }
+        }
         
         // is sound ready
         [[player currentItem] addObserver:self
